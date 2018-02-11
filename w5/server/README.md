9ad4f6282a6181105d0ac8dab279a062a091d9c0
bobur554395 <bobur.muhsimbaev@gmail.com>
2018-02-12 01:07:25 +0600

    asd

---
+++ w5/server/README.md
@@ -0,0 +1,4 @@
+
+## NodeJS server example
+
+- [Updating to New Releases](#updating-to-new-releases)
---
+++ w5/timer_tracker/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app-1';
+
+ReactDOM.render(<App />, document.getElementById('root'));
